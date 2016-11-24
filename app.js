f790f83cbd383ca75373847d1dcfc6486b73b793
ironthrone <laozhuangg@gmail.com>
2016-11-24 14:48:47 +0800

    fix interface

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
 // var path = 
-var comment = require('./routes/comment.js')
 
 var mongoUrl = (process.env.MONGODB_URI || 'mongodb://localhost:27017/mean');
 var port = (process.env.PORT || 5000);
@@ -10,10 +9,80 @@ var port = (process.env.PORT || 5000);
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/comment',comment);
+
+
+var db;
+var COMMENT_COLLECTION = "comment";
+
+mongodb.MongoClient.connect(mongoUrl,function(err,database){
+	if(err){
+		console.log(err);
+		process.exit(1);
+	}
+
+	db = database;
+
+    var server = app.listen(port, function () {
+    	var port = server.address().port;
+        console.log('Started,now  listening on port ',port);
+    });
+
+});
+
+
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  var result = {};
+  result.status = code;
+  result.message = message;
+  result.results = null;
+  res.status(code || 500).json(result);
+}
 
 
 
-app.listen(3000, function () {
-    console.log('Started,now  listening on port 3000!');
+/*
+ comment :{
+	contentId:<String>
+	content: <String>
+	id:<ObjectId>
+	timestamp:<Date>
+ }
+ return: {
+    status:<int>,
+    message:<String>,
+    results:<Object or Array>
+ }
+*/
+app.post('/add',function (req,res) {
+    var comment = req.body;
+    comment.timestamp = Date.now();
+
+    if(!comment.contentId) {
+        handleError(res,"Invalid user input","contentId can not be null",400);
+        return;
+
+    }
+    if(!comment.content){
+        handleError(res,"Invalid user input","Content can not be null",400);
+        return;
+    }
+    
+    db.collection(COMMENT_COLLECTION).insertOne(comment, function(err,doc){
+        if(err){
+            handleError(res,err.message,"Fail to create new comment",500);
+        }else {
+            res.status(200).json({status:1,message:"Success",result:null});
+        }
+    });
+})
+
+app.get('/list',function (req,res) {
+    db.collection(COMMENT_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.",500);
+    } else {
+      res.status(200).json({status:1,message:null,results:docs});
+    }
+  });
 });
